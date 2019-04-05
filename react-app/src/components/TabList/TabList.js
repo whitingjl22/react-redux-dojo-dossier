d9c5defec6fe19aeccb78284a0bd08c7e6b39041
WHITING, JEREMY <jeremy.whiting23@gmail.com>
2019-04-05 08:11:07 -0500

    update

--- react-app/src/components/TabList/TabList.js
+++ react-app/src/components/TabList/TabList.js
@@ -1,26 +1,20 @@
 import React from "react"
 import { connect } from "react-redux"
 import { viewProfile } from "../../redux"
-import DossierEntry from "../DossierEntry/DossierEntry"
 import "./TabList.css"
 
 const TabList = (props) => {
   // const TaskList = (props) => {
   // const tasks = props.tasks
 
-  let dossiers = props.tasks.map((task, idx) => <DossierEntry task={task} key={idx} />)
-
   console.log("tab list props: ", props)
   return (
     <div className="tabListContainer">
-      {/* {props.tasks.map((task, index) => (
-        <div style={{ display: "inline-block" }} key={index}>
-          <ul>
-            <li>{task.title}</li>
-          </ul>
-        </div>
-			))} */}
-      <ul>{dossiers}</ul>
+      {props.tasks.map((task, index) => (
+        <ul key={index}>
+          <li onClick={() => props.viewProfile(task.id)}>{task.title}</li>
+        </ul>
+      ))}
     </div>
   )
 }
