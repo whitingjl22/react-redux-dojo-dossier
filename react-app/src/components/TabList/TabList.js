b5bcef5f711cf962ddd084a203f4b1baf90e6529
WHITING, JEREMY <jeremy.whiting23@gmail.com>
2019-04-06 10:50:07 -0500

    update

--- react-app/src/components/TabList/TabList.js
+++ react-app/src/components/TabList/TabList.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { connect } from "react-redux"
 import { viewProfile, updateTasksList } from "../../redux"
-import { retrieveTasksPromise } from "../../helper"
+// import { retrieveTasksPromise } from "../../helper"
 import axios from "axios"
 import "./TabList.css"
 
